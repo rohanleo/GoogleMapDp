299f99adc2c4bc278449a43cab4c32f6b27d8ccb
rohanleo <rohansingh726@gmail.com>
2020-04-02 12:23:47 +0530

    bug fixes

--- app/src/main/java/com/example/map/LoginActivity.java
+++ app/src/main/java/com/example/map/LoginActivity.java
@@ -1,55 +1,81 @@
 package com.example.map;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    //var
+    private boolean mLocationPermissionGranted = false;
 
     private SharedPreferences sp;
     private Button submit;
-    public static EditText userName;
-    public static EditText userPhoneNum;
+    public static String userName;
+    public static String userPhoneNum;
+    private EditText unm;
+    private EditText phone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sp = getSharedPreferences("First log",0);
+        getLocationPermission();
         if(sp.getString("First login","").toString().equals("no")){
+            userName = sp.getString("unm","");
+            userPhoneNum = sp.getString("phone","");
             Intent i = new Intent(LoginActivity.this,MapActivity.class);
             startActivity(i);
             finish();
         }else{
             setContentView(R.layout.activity_splash);
-            userName = findViewById(R.id.nameText);
-            userPhoneNum = findViewById(R.id.phoneText);
+            unm = findViewById(R.id.nameText);
+            phone = findViewById(R.id.phoneText);
             submit = findViewById(R.id.submitButton);
             submit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(googleServiceAvailable()){
-                        if(userName.getText().toString().matches("") || userPhoneNum.getText().toString().matches("")){
+                    if(googleServiceAvailable() && mLocationPermissionGranted){
+                        if(unm.getText().toString().matches("") || phone.getText().toString().matches("")){
                             Toast.makeText(LoginActivity.this,"Please fill the details",Toast.LENGTH_LONG).show();
                         }else{
                             SharedPreferences.Editor editor = sp.edit();
                             editor.putString("First login","no");
+                            editor.putString("unm", String.valueOf(unm.getText()));
+                            editor.putString("phone", String.valueOf(phone.getText()));
                             editor.commit();
+                            userName = sp.getString("unm","");
+                            userPhoneNum = sp.getString("phone","");
                             Intent i = new Intent(LoginActivity.this,MapActivity.class);
                             startActivity(i);
                             finish();
                         }
+                    }
+                    else{
+                        //Toast.makeText(LoginActivity.this,"Please agree to all Permission Requests",Toast.LENGTH_LONG).show();
+                        getLocationPermission();
                     }
                 }
             });
@@ -71,5 +97,54 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this,"Cant connect to play Service", Toast.LENGTH_LONG).show();
         }
         return false;
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG,"getLocationPermission: getting location permissions");
+
+        String[] permissions = {android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted = true;
+                //Log.d(TAG,"getLocationPermission: getting location permissions 1" + mLocationPermissionGranted );
+                //initMap();
+            }
+            else{
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+                //Log.d(TAG,"getLocationPermission: getting location permissions 2" + mLocationPermissionGranted );
+            }
+        }
+        else{
+            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            //Log.d(TAG,"getLocationPermission: getting location permissions 3" + mLocationPermissionGranted );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG,"onRequestPermissionsResult: called");
+
+        mLocationPermissionGranted = false;
+
+        switch (requestCode)
+        {
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    for (int i=0;i<grantResults.length;i++){
+                        if (grantResults[i]!= PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted=false;
+                            Log.d(TAG,"onRequestPermissionsResult: permission failed");
+                            Toast.makeText(this, "Location Permission denied", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    Log.d(TAG,"onRequestPermissionsResult: permission granted");
+                    mLocationPermissionGranted=true;
+                    //Log.d(TAG,"onRequestPermissionsResult: permission granted 1" + mLocationPermissionGranted);
+                    //initMap();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/map/MapActivity.java
+++ app/src/main/java/com/example/map/MapActivity.java
@@ -13,6 +13,7 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 
@@ -67,37 +68,30 @@ import java.util.Objects;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "MapActivity";
 
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-
     //Widgets
     private SearchView mSearchText;
     private ImageView mgps, mInfo;
+    private Button addMarker;
 
-    //var
-    private boolean mLocationPermissionGranted = false;
     GoogleMap mMap;
     private FusedLocationProviderClient mfusedLocationProviderClient;
-    //private Location mLastKnownLocation;
-    //private GoogleApiClient mGoogleApiClient;
 
     private Marker marker = null;
     DatabaseReference databaseMarkers;
-    //private ArrayList<Marker> markerList;
 
-    private boolean isInfoDiplayed = false, isInfoAvailable = false;
+    private boolean isInfoDiplayed = false,addingMarkerEnabled=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         databaseMarkers= FirebaseDatabase.getInstance().getReference("geojson");
-            mSearchText = findViewById(R.id.input_search);
-            mgps = findViewById(R.id.ic_gps);
-            mInfo = findViewById(R.id.ic_info);
-            getLocationPermission();
+        mSearchText = findViewById(R.id.input_search);
+        mgps = findViewById(R.id.ic_gps);
+        mInfo = findViewById(R.id.ic_info);
+        addMarker = findViewById(R.id.addMarker);
+        addingMarkerEnabled = false;
+        initMap();
     }
 
     @Override
@@ -106,15 +100,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Log.d(TAG, "OnMapReady: Map is Ready");
 
         mMap = googleMap;
-        if(mLocationPermissionGranted){
             getDeviceLocation();
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
-            //mMap.getUiSettings().setMapToolbarEnabled(true);
             init();
             clickMap();
-        }
-
     }
 
     @Override
@@ -130,7 +120,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                             .title("Details Available");
                     Marker mk = mMap.addMarker(options);
                     mk.setDraggable(true);
-                    //markerList.add(mk);
                     mk.setTag(info);
                 }
             }
@@ -146,22 +135,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                Geocoder geocoder = new Geocoder(MapActivity.this);
-                List<Address> list = new ArrayList<>();
-                try {
-                    list = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
-                }catch (IOException e){
-                    Log.e(TAG,"clickMap: Map is clicked" + e.getMessage());
-                }
-                if(list.size()>0){
-                    Address address = list.get(0);
-                    Log.d(TAG,"geolocate: found a location: " + address.toString());
-                    //Toast.makeText(MapActivity.this,address.toString(),Toast.LENGTH_LONG).show();
-                    MarkerOptions options = new MarkerOptions().position(latLng).title("No Details");
-                    Marker mk = mMap.addMarker(options);
-                    mk.setDraggable(true);
-                    //markerList.add(mk);
-                    //moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),15f,address.getAddressLine(0));
+                if(addingMarkerEnabled){
+                    Geocoder geocoder = new Geocoder(MapActivity.this);
+                    List<Address> list = new ArrayList<>();
+                    try {
+                        list = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+                    }catch (IOException e){
+                        Log.e(TAG,"clickMap: Map is clicked" + e.getMessage());
+                    }
+                    if(list.size()>0){
+                        Address address = list.get(0);
+                        Log.d(TAG,"geolocate: found a location: " + address.toString());
+                        //Toast.makeText(MapActivity.this,address.toString(),Toast.LENGTH_LONG).show();
+                        MarkerOptions options = new MarkerOptions().position(latLng).title("No Details");
+                        Marker mk = mMap.addMarker(options);
+                        mk.setDraggable(true);
+                    }
                 }
             }
         });
@@ -229,7 +218,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .withDarkerOverlay(true)
                 .build();
 
-        Button btnsave, btndel,chat;
+        final Button btnsave, btndel,chat;
+        btnsave = view.findViewById(R.id.save);
+        btndel = view.findViewById(R.id.delete);
         final EditText name = view.findViewById(R.id.name1);
         final Spinner type = view.findViewById(R.id.type1);
         final EditText opening = view.findViewById(R.id.openingTime1);
@@ -245,51 +236,62 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
         else
         {
+            btndel.setText("Chat Room");
+            btnsave.setText("Edit Details");
             info= (InfoWindowData) marker.getTag();
             name.setText(info.getName());
+            name.setInputType(InputType.TYPE_NULL);
             opening.setText(info.getOpening());
+            opening.setInputType(InputType.TYPE_NULL);
             closing.setText(info.getClosing());
+            closing.setInputType(InputType.TYPE_NULL);
             phoneNum.setText(info.getPhoneNum());
+            phoneNum.setInputType(InputType.TYPE_NULL);
             remark.setText(info.getRemark());
+            remark.setInputType(InputType.TYPE_NULL);
             type.setSelection(((ArrayAdapter<String>)type.getAdapter()).getPosition(info.getType()));
         }
-
-        btnsave = view.findViewById(R.id.save);
-        btndel = view.findViewById(R.id.delete);
-        chat = view.findViewById(R.id.chat);
-        chat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         btndel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                marker.remove();
-                //markerList.remove(marker);
-                databaseMarkers.child(info.getId()).removeValue();
-                materialStyledDialog.dismiss();
+                if(btndel.getText().equals("Chat Room")){
+                    Toast.makeText(MapActivity.this,"This section is under maintenance, Please wait ;)",Toast.LENGTH_LONG).show();
+                }else{
+                    marker.remove();
+                    //markerList.remove(marker);
+                    databaseMarkers.child(info.getId()).removeValue();
+                    materialStyledDialog.dismiss();
+                }
             }
         });
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                info.setName(name.getText().toString());
-                info.setType(type.getSelectedItem().toString());
-                info.setOpening(opening.getText().toString());
-                info.setClosing(closing.getText().toString());
-                info.setPhoneNum(phoneNum.getText().toString());
-                info.setAddedby(LoginActivity.userName.getText().toString());
-                info.setAddedon(java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime()));
-                info.setRemark(remark.getText().toString());
-                info.setLat(marker.getPosition().latitude);
-                info.setLng(marker.getPosition().longitude);
-                databaseMarkers.child(info.getId()).setValue(info);
-                Toast.makeText(MapActivity.this, "Details Saved", Toast.LENGTH_SHORT).show();
-                materialStyledDialog.dismiss();
-                marker.hideInfoWindow();
-                marker.remove();
+                if(btnsave.getText().equals("Save Changes")){
+                    info.setName(name.getText().toString());
+                    info.setType(type.getSelectedItem().toString());
+                    info.setOpening(opening.getText().toString());
+                    info.setClosing(closing.getText().toString());
+                    info.setPhoneNum(phoneNum.getText().toString());
+                    info.setAddedby(LoginActivity.userName);
+                    info.setAddedon(java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime()));
+                    info.setRemark(remark.getText().toString());
+                    info.setLat(marker.getPosition().latitude);
+                    info.setLng(marker.getPosition().longitude);
+                    databaseMarkers.child(info.getId()).setValue(info);
+                    Toast.makeText(MapActivity.this, "Details Saved", Toast.LENGTH_SHORT).show();
+                    materialStyledDialog.dismiss();
+                    marker.hideInfoWindow();
+                    marker.remove();
+                }else{
+                    btndel.setText("Delete Marker");
+                    btnsave.setText("Save Changes");
+                    name.setInputType(InputType.TYPE_CLASS_TEXT);
+                    opening.setInputType(InputType.TYPE_CLASS_TEXT);
+                    closing.setInputType(InputType.TYPE_CLASS_TEXT);
+                    phoneNum.setInputType(InputType.TYPE_CLASS_TEXT);
+                    remark.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
             }
         });
         materialStyledDialog.show();
@@ -334,6 +336,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 materialStyledDialog.show();
             }
         });
+        addMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(addMarker.getText().equals("Add")){
+                    addingMarkerEnabled=true;
+                    addMarker.setText("Disable Add");
+                }else{
+                    addingMarkerEnabled =false;
+                    addMarker.setText("Add");
+                }
+            }
+        });
     }
 
     private void geoLocate(){
@@ -366,81 +380,23 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(MapActivity.this);
     }
 
-    private void getLocationPermission(){
-        Log.d(TAG,"getLocationPermission: getting location permissions");
-
-        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED)
-        {
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-                mLocationPermissionGranted = true;
-                //Log.d(TAG,"getLocationPermission: getting location permissions 1" + mLocationPermissionGranted );
-                initMap();
-            }
-            else{
-                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
-                //Log.d(TAG,"getLocationPermission: getting location permissions 2" + mLocationPermissionGranted );
-            }
-        }
-        else{
-            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
-            //Log.d(TAG,"getLocationPermission: getting location permissions 3" + mLocationPermissionGranted );
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG,"onRequestPermissionsResult: called");
-
-        mLocationPermissionGranted = false;
-
-        switch (requestCode)
-        {
-            case LOCATION_PERMISSION_REQUEST_CODE:{
-                if(grantResults.length>0){
-                    for (int i=0;i<grantResults.length;i++){
-                        if (grantResults[i]!=PackageManager.PERMISSION_GRANTED){
-                            mLocationPermissionGranted=false;
-                            Log.d(TAG,"onRequestPermissionsResult: permission failed");
-                            Toast.makeText(this, "Location Permission denied", Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                    }
-                    Log.d(TAG,"onRequestPermissionsResult: permission granted");
-                    mLocationPermissionGranted=true;
-                    //Log.d(TAG,"onRequestPermissionsResult: permission granted 1" + mLocationPermissionGranted);
-                    initMap();
-                }
-            }
-        }
-    }
-
     private void getDeviceLocation(){
         Log.d(TAG,"getDeviceLocation: getting the device current location");
         mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         try {
-            if (mLocationPermissionGranted){
                 Task location = mfusedLocationProviderClient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(@NonNull Task task) {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "onComplete: Found Location");
-                            Location location = (Location) task.getResult();
-                            /*if(location == null){
-                                location.setLatitude(20.5937);
-                                location.setLongitude(78.9629);
-                                moveCamera(new LatLng(location.getLatitude(),location.getLongitude()), 6f,"My location");
-                            }
-                            else*/ moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), 15f, "My location");
+                            Location location = (Location) task.getResult();moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), 15f, "My location");
                         } else {
                             Log.d(TAG, "onComplete: current location is null");
                             Toast.makeText(MapActivity.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-            }
-
         }catch (SecurityException e)
         {
             Log.d(TAG,"getDeviceLocation: SecurityException" + e.getMessage());
