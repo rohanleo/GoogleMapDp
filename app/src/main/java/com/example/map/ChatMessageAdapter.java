dc9bccdfe6ae27e30d269ef6582d6b58a22950b3
rohanleo <rohansingh726@gmail.com>
2020-04-03 01:35:43 +0530

    chat room

---
+++ app/src/main/java/com/example/map/ChatMessageAdapter.java
@@ -0,0 +1,39 @@
+package com.example.map;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
+
+    private Activity context;
+    private List<ChatMessage>chat;
+
+    public ChatMessageAdapter(Activity context,List<ChatMessage>chat){
+        super(context,R.layout.message_layout,chat);
+        this.context=context;
+        this.chat=chat;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        View listViewItem = layoutInflater.inflate(R.layout.message_layout,null,true);
+        TextView sender = listViewItem.findViewById(R.id.sender);
+        TextView message = listViewItem.findViewById(R.id.msg);
+
+        ChatMessage chatMessage = chat.get(position);
+        sender.setText(chatMessage.getSender());
+        message.setText(chatMessage.getMessage());
+        return listViewItem;
+    }
+}
